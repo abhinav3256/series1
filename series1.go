878b95fc6b5ae3161c66db5cc3045d0547029456
abhinav3256 <abhinavkumar3256@gmail.com>
2022-06-13 15:15:22 +0530

    adding number series file

--- series1.go
+++ series1.go
@@ -9,21 +9,18 @@ func main() {
 	no_of_digit := 0
 	fmt.Println("Enter a digit")
 	fmt.Scanln(&no_of_digit)
-
 	if no_of_digit == 1 {
 		fmt.Println(a)
 	} else {
-		fmt.Print(a, ",", b)
+		fmt.Println(a)
+
+		fmt.Println(b)
 	}
 
 	for i := 3; i <= no_of_digit; i++ {
 		next_num := a + b
 
-		if i == no_of_digit {
-			fmt.Println(",", next_num)
-		} else {
-			fmt.Print(",", next_num)
-		}
+		fmt.Println(next_num)
 
 		a = b
 		b = next_num
